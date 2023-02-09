c6f137d6a11467c8ddea0456b6d41569ff8e24f1
OmerMohideen <omermohideen2004@gmail.com>
2023-02-09 14:12:34 +0530

    fixed favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Programming Quotes API</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/logo.png" />
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Body from "./components/Body";
 
 function page() {
 
-
   return (
     <div>
       <Header />
