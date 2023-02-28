f8ddf0c04eaa3a1b27362547f87b02904e199a92
OmerMohideen <omermohideen2004@gmail.com>
2023-02-28 23:51:37 +0530

    CORS 💀

--- src/routes/bulk.ts
+++ src/routes/bulk.ts
@@ -3,9 +3,10 @@ import path from "path";
 import data from "../../data.json";
 import { StatusCodes } from "http-status-codes";
 const router = new Router();
+import cors from "@koa/cors";
 
 router.get(
-  `/${path.basename(__filename, path.extname(__filename))}`,
+  `/${path.basename(__filename, path.extname(__filename))}`, cors(),
   async (ctx) => {
     const {
       query: { author },
